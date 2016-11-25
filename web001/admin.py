7ba5b1ec4bacf415f04a29ca2ed4c127e99e0479
tom <qdtom@foxmail.com>
2016-11-25 23:13:56 +0800

    add Posts model

--- web001/admin.py
+++ web001/admin.py
@@ -12,5 +12,11 @@ class TitleAdmin(admin.ModelAdmin):
     search_fields = ['enName', 'cnName']
 
 
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('post_title', 'post_content')
+    search_fields = ['post_title', 'post_content']
+
+
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Titles, TitleAdmin)
+admin.site.register(Posts, PostsAdmin)
--- web001/models.py
+++ web001/models.py
@@ -16,3 +16,35 @@ class Titles(models.Model):
 
     def __unicode__(self):  # __str__ on Python 3
         return self.enName  # 在admin配置页面，可以容易的看到名
+
+
+post_date = models.IntegerField()
+meta_key = models.CharField(max_length=255)
+
+
+class Posts(models.Model):
+    post_author = models.CharField(max_length=255)
+    post_date = models.CharField(max_length=255)
+    post_date_gmt = models.CharField(max_length=255)
+    post_content = models.TextField()
+    post_title = models.CharField(max_length=255)
+    post_excerpt = models.CharField(max_length=255)
+    post_status = models.CharField(max_length=255)
+    comment_status = models.CharField(max_length=255)
+    ping_status = models.CharField(max_length=255)
+    post_password = models.CharField(max_length=255)
+    post_name = models.CharField(max_length=255)
+    to_ping = models.CharField(max_length=255)
+    pinged = models.CharField(max_length=255)
+    post_modified = models.CharField(max_length=255)
+    post_modified_gmt = models.CharField(max_length=255)
+    post_content_filtered = models.CharField(max_length=255)
+    guid = models.CharField(max_length=255)
+    menu_order = models.IntegerField()
+    post_type = models.CharField(max_length=255)
+    post_mime_type = models.CharField(max_length=255)
+    comment_count = models.IntegerField()
+
+
+def __unicode__(self):  # __str__ on Python 3
+    return self.post_title  # 在admin配置页面，可以容易的看到名
